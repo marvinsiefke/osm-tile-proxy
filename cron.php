688c45842aa1a4230d6426a47c22fa614cc57afc
Marvin Siefke <119980601+marvinsiefke@users.noreply.github.com>
2024-05-31 20:22:03 +0200

    Update cron.php

--- cron.php
+++ cron.php
@@ -1,9 +1,5 @@
 <?php
 
-if (php_sapi_name() !== 'cli') {
-	die('This file can only be run from the command line.');
-}
-
 ini_set('display_errors', 0);
 ini_set('log_errors', 1);
 ini_set('max_execution_time', 600);
@@ -11,5 +7,13 @@ ini_set('max_execution_time', 600);
 require 'config/config.php';
 require 'src/tileProxy.php';
 
-$tileProxy = new tileProxy($operator, $trustedHosts);
+if ($config['cron'] !== true) {
+	die('Cron is not allowed.');
+}
+
+if ($config['forceCli'] === true && php_sapi_name() !== 'cli') {
+	die('This file can only be run from the command line.');
+}
+
+$tileProxy = new tileProxy($config['operator'], $config['trustedHosts'], $config['cron'], $config['browserTtl'], $config['tileserver'], $config['tolerance'], $config['storage']);
 $tileProxy->processQueue();
