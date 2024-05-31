38250bbfbcb55b51b8cf3ccca50ebb04c675272e
Marvin Siefke <119980601+marvinsiefke@users.noreply.github.com>
2024-05-31 20:24:31 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,12 +1,38 @@
 <?php
 
 // Proxy settings
-$operator = 'admin@domain.com';
-$trustedHosts = [
+$config['operator'] = 'admin@domain.com';
+$config['cron'] = true;
+$config['forceCli'] = true;
+$config['browserTtl'] = 86400 * 7;
+$config['tolerance'] = 0.5;
+$config['storage'] = 'tmp/';
+$config['tileservers'] = [
+	'openstreetmap' => [
+		'urls' => 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+		'ttl' => 86400 * 45,
+		'batch' => 25
+	],
+	'maptiler-osm' => [
+		'urls' => 'https://api.maptiler.com/maps/openstreetmap/256/{z}/{x}/{y}@2x.jpg?key={KEY}',
+		'ttl' => 86400 * 45,
+		'batch' => 50
+	],
+	'maptiler-streets' => [
+		'urls' => 'https://api.maptiler.com/maps/streets-v2/256/{z}/{x}/{y}@2x.png?key={KEY}',
+		'ttl' => 86400 * 45,
+		'batch' => 50
+	],
+	'maptiler-basic' => [
+		'urls' => 'https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}@2x.png?key={KEY}',
+		'ttl' => 86400 * 45,
+		'batch' => 50
+	],
+];
+$config['trustedHosts'] = [
 	'proxy.domain.de' => [
-		'referers' => [
-			'domain.de' // allowed referers
-		],
+		'tileserver' => 'openstreetmap',
+		'referers' => 'domain.de',
 		'maxBounds' => [ 
 			[47.25, 5.875], // south west
 			[53.05, 13.85] // north east
