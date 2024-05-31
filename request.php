3394e1b5cbb8c3382c5c8fb3f247547a1310fca6
Marvin Siefke <119980601+marvinsiefke@users.noreply.github.com>
2024-05-31 20:21:48 +0200

    Update request.php

--- request.php
+++ request.php
@@ -9,11 +9,10 @@
 
 ini_set('display_errors', 0);
 ini_set('log_errors', 1);
-ini_set('max_execution_time', 30);
+ini_set('max_execution_time', 60);
 
 require 'config/config.php';
-require 'src/rateLimiter.php';
 require 'src/tileProxy.php';
 
-$tileProxy = new tileProxy($operator, $trustedHosts);
+$tileProxy = new tileProxy($config['operator'], $config['trustedHosts'], $config['cron'], $config['browserTtl'], $config['tileservers'], $config['tolerance'], $config['storage']);
 $tileProxy->processRequest();
